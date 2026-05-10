1c7b6dae15e68afb4dbf6093a7c4916638f31d45
vishudverma <244vishudverma@gmail.com>
2026-05-10 22:39:32 +0000

    moved the server-python-sdk to fabric-sdk-py directory

--- server-python-sdk/load_connection_profile.py
+++
@@ -1,8 +0,0 @@
-from hfc.fabric import Client
-
-cli = Client(net_profile="../fabric-sdk-py/test/fixtures/network.json")
-
-print(cli.organizations)
-print(cli.peers)
-print(cli.orderers)
-print(cli.CAs)
